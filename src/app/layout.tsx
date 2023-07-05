ada35a5496b4f00ecb0323fe076a70226ddca3f9
Rene van Dijk <renevandijk@Renes-MBP.domain_not_set.invalid>
2023-07-05 18:12:36 +0200

    step

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" data-theme="aqua">
+      <body className="min-h-screen bg-white flex justify-center align-middle">{children}</body>
     </html>
   )
 }
